df768fbaa75520a082d9f79bc8bf29719afbc5ac
jas-singh-code <jaspreetsingh7798@gmail.com>
2021-02-08 17:46:33 -0500

    trying to debug sessioncontroller create and Usercontroller create

--- 99cats_Auth/app/controllers/users_controller.rb
+++ 99cats_Auth/app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController #appcontroller needs login method which checks if session token 
     #is same as what user input
+    # def show
+    #     @user = User.find(params[:id])
+    #     render :show
+    # end
 
     def new
         @user = User.new
@@ -9,7 +13,7 @@ class UsersController < ApplicationController #appcontroller needs login method 
     def create  #sessio conr=troller needs to have session token
         @user = User.new(user_params)
         if @user.save
-            redirect_to user_url(@user)
+            redirect_to cats_url
         else
             render json: @users.errors.full_messages, status: 422 
         end
