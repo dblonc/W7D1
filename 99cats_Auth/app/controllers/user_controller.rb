12b200f3f81a537cd437b569d550240a624821f6
Daniel Bloncourt <dblonc@aol.com>
2021-02-08 15:00:04 -0500

    Started Controllers and starting views

--- 99cats_Auth/app/controllers/user_controller.rb
+++ 99cats_Auth/app/controllers/user_controller.rb
@@ -1,1 +1,33 @@
-UsersContr
+class UsersController < ApplicationController
+
+    def show
+        @users = User.all 
+        render :show
+
+    end
+
+    def new
+        @user = User.new(user_params)
+        render :new
+
+    end
+
+    def create
+        @user = User.new(user_params)
+        if user.save
+            render :new
+        else
+            render :json @users.errors.full_messages, status: 422 
+        end
+        render :new
+
+    end
+
+    private
+
+    def user_params
+        params.require(:@users).permit(:username)
+    end
+
+
+end
